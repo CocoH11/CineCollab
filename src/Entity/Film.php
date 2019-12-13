d8d18b23a2ee8d267c13d2f82aff529dca1f861c
corentin <corentin.holcvart@hotmail.fr>
2019-12-13 16:47:31 +0100

    Première entité film qui hérite d'oeuvre

---
+++ src/Entity/Film.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FilmRepository")
+ */
+class Film extends Oeuvre
+{
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $resume;
+
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+}
